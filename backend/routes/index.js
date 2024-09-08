797c02fa93a262b5772763e98d16359e3a562f57
Anusha <158035121+Anushabore60@users.noreply.github.com>
2024-09-08 17:14:36 +0530

    Update index.js
    deleted unnecessary data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,6 @@ const accountroutes=require('./account')
 const router =express.Router()
 router.use ('/user',userroutes)
 router.use('/account',accountroutes)
-//router.use ('/user',userroutes)
 
 
 
